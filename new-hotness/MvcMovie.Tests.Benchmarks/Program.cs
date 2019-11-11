0dc0ee27756387692f2370408884c0ae864828ff
Sean Killeen <SeanKilleen@gmail.com>
2019-11-11 15:17:46 -0500

    Create test projects

---
+++ new-hotness/MvcMovie.Tests.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvcMovie.Tests.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
