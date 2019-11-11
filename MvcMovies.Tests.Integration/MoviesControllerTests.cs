d2cc4b23cb12c537334fe3c3a154f9ce1a9249a7
Sean Killeen <SeanKilleen@gmail.com>
2019-11-11 09:59:33 -0500

    Add integration tests

---
+++ MvcMovies.Tests.Integration/MoviesControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MvcMovie.Controllers;
+using MvcMovie.Models;
+using Xunit;
+
+namespace MvcMove.Tests.Integration
+{
+    public class MovieControllerTests : IDisposable
+    {
+        private readonly MoviesController _sut;
+        private readonly MovieDBContext _dbContext;
+        private readonly DbContextTransaction _transaction;
+
+        public MovieControllerTests()
+        {
+            _dbContext = new MovieDBContext();
+            _transaction = _dbContext.Database.BeginTransaction();
+
+            var aRomanceMovie = new Movie { ID = 1, Genre = "Romance", Price = 1.00m, Rating = "A+", ReleaseDate = DateTime.Now, Title = "A Romance Movie" };
+            var romeoAndJuliet = new Movie { ID = 2, Genre = "Romance", Price = 1.00m, Rating = "A+", ReleaseDate = DateTime.Now, Title = "Romeo and Juliet" };
+            var zoolander = new Movie { ID = 3, Genre = "Comedy", Price = 1.00m, Rating = "A+", ReleaseDate = DateTime.Now, Title = "Romeo and Juliet" };
+            var aComedyMovie = new Movie { ID = 4, Genre = "Comedy", Price = 1.00m, Rating = "A+", ReleaseDate = DateTime.Now, Title = "A Comedy Movie" };
+
+            _dbContext.Movies.Add(aRomanceMovie);
+            _dbContext.Movies.Add(romeoAndJuliet);
+            _dbContext.Movies.Add(zoolander);
+            _dbContext.Movies.Add(aComedyMovie);
+
+            _sut = new MoviesController(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            _transaction.Rollback();
+        }
+
+        [Fact]
+        public void DBStartsWithThreeMovies()
+        {
+            _dbContext.Movies.ToList().Count.Should().Be(2);
+        }
+    }
+}
