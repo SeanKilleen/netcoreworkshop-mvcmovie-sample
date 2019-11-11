9159da8c8a8c710ef4241803bdf4b62cd750dfb1
Sean Killeen <SeanKilleen@gmail.com>
2019-11-11 13:40:40 -0500

    Setup Autofac DI

--- MvcMovie/Global.asax.cs
+++ MvcMovie/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace MvcMovie
 {
@@ -12,10 +14,44 @@ namespace MvcMovie
     {
         protected void Application_Start()
         {
+            var containerBuilder = CreateContainerBuilder();
+
+            // Set the dependency resolver to be Autofac.
+            var container = containerBuilder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private ContainerBuilder CreateContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterAssemblyTypes(
+                    typeof(MvcApplication).Assembly)
+                .AsImplementedInterfaces();
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            return builder;
         }
     }
 }
