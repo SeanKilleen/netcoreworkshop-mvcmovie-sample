b8d8aa56a63ec9f023d5bd83fdf29433edface45
Sean Killeen <SeanKilleen@gmail.com>
2019-11-11 17:31:22 -0500

    copy program as is

--- new-hotness/MvcMovie.Tests.Benchmarks/Program.cs
+++ new-hotness/MvcMovie.Tests.Benchmarks/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using MvcMovie.Controllers;
+using MvcMovie.Models;
 
-namespace MvcMovie.Tests.Benchmarks
+namespace MvcMovies.Tests.Benchmarks
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<SearchTests>();
+        }
+    }
+
+    [HtmlExporter]
+    [MemoryDiagnoser]
+    public class SearchTests
+    {
+        private readonly int NumberOfSearches = 25;
+
+        [Benchmark]
+        public void WriteAllMovies()
+        {
+            var movieDbContext = new MovieDBContext();
+            var controller = new MoviesController(movieDbContext);
+
+            for (var i = 1; i <= NumberOfSearches; i++)
+            {
+                var result = controller.Index(movieGenre:null, searchString:null) as ViewResult;
+
+                var model = result.Model as IEnumerable<Movie>;
+
+                foreach (var movie in model)
+                {
+                    Console.WriteLine($"Result {movie.Title}");
+                }
+            }
+        }
+
+        [Benchmark]
+        public void SearchByGenre()
+        {
+            var movieDbContext = new MovieDBContext();
+            var controller = new MoviesController(movieDbContext);
+
+            var movieGenre = "Comedy";
+            for (var i = 1; i <= NumberOfSearches; i++)
+            {
+                var result = controller.Index(movieGenre: movieGenre, searchString: null) as ViewResult;
+
+                var model = result.Model as IEnumerable<Movie>;
+
+                foreach (var movie in model)
+                {
+                    Console.WriteLine($"Result {movie.Title}");
+                }
+            }
+        }
+        [Benchmark]
+        public void SearchByTitle()
+        {
+            var movieDbContext = new MovieDBContext();
+            var controller = new MoviesController(movieDbContext);
+
+            var movieTitle = "Ghostbusters";
+            for (var i = 1; i <= NumberOfSearches; i++)
+            {
+                var result = controller.Index(movieGenre: null, searchString: movieTitle) as ViewResult;
+
+                var model = result.Model as IEnumerable<Movie>;
+
+                foreach (var movie in model)
+                {
+                    Console.WriteLine($"Result {movie.Title}");
+                }
+            }
         }
     }
 }
