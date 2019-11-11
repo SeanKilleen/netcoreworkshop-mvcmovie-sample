49a13b2d9de37a7dcc36c44e0f57535c85f9c534
Sean Killeen <SeanKilleen@gmail.com>
2019-11-11 16:10:27 -0500

    Remove extra space

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,37 +10,22 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
 
-
-
-
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
-
-
-
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
-
-
-
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [Required]
         [StringLength(30)]
         public string Genre { get; set; }
 
-
-
-
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
-
-
-
 
         [StringLength(5)]
         public string Rating { get; set; }
